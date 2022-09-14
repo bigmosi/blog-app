74fe75cab9b65310314e9780f3e4638870e36da7
bigmosi <kinyeramo@gmail.com>
2022-09-14 13:01:46 +0300

    fixed linters errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+# rubocop:disable Style/Documentation
 
 ActiveRecord::Schema[7.0].define(version: 20_220_727_130_516) do
   # These are extensions that must be enabled in order to support this database
