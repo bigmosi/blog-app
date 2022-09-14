d1b301a37ea8643710321f6ee482058ddae60547
bigmosi <kinyeramo@gmail.com>
2022-09-14 12:45:41 +0300

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -35,17 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 20_220_727_130_516) do
     t.index ['post_id'], name: 'index_likes_on_post_id'
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title'
-    t.string 'text'
-    t.integer 'comments_counter'
-    t.integer 'likes_counter'
-    t.bigint 'author_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_posts_on_author_id'
-  end
-
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'photo'
